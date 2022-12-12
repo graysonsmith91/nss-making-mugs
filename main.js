e8d7ec5e505075db508fce6646be014a651329fb
Grayson Smith <graysonsmith91@gmail.com>
2022-12-12 10:24:48 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const copy of clay) {
+   const mug = `${copy} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
